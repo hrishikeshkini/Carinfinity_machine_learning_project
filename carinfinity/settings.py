ebf0aad1d327eb9c55a81a2880ac3793fbec5fca
Hrishikesh Kini <47620069+hrishikeshkini@users.noreply.github.com>
2021-07-29 11:41:57 +0530

    Update settings.py

--- carinfinity/settings.py
+++ carinfinity/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lg#&^xxr0=t2(#bg($h*%x967=g1w-k97!2ej1&3&g6r=s%vaj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'carinfinity.herokuapp.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
